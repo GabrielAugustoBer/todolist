496cfac4ddbe36b20b39055f256e0f35357bfedf
Gabriel Augusto <92814110+GabrielAugustoBer@users.noreply.github.com>
2023-10-09 14:19:17 -0300

    primeira aula

---
+++ src/main/java/com/gabrielaugusto/todolist/user/UserController.java
@@ -0,0 +1,35 @@
+package com.gabrielaugusto.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /*
+     * String (texto)
+     * Integer (int) Números inteiros
+     * double (double) Números 0.0000
+     * Float (float) Números 0.000
+     * char (A C)
+     * Date (data)
+     * void
+     */
+
+     /*
+      * Body
+      */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println("O nome do usuario é:" + userModel.name + "Username:" + userModel.username);
+    }
+}
---
+++ src/main/java/com/gabrielaugusto/todolist/user/UserModel.java
@@ -0,0 +1,9 @@
+package com.gabrielaugusto.todolist.user;
+
+public class UserModel {
+   public String username, name, password; 
+
+
+
+
+}
